214c2c782181af4b5d517b3101dfff20a9d56e7b
ShunyaSuzuki <anyushu2017@gmail.com>
2019-12-12 18:12:27 +0900

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,4 @@ import '../scss/style.scss';
 
 // JSインポート
 import '@fortawesome/fontawesome-free/js/all.js';
-import './plugins/bootstrap-material-design.min.js';
-import './plugins/nouislider.min.js';
-import './material-kit.js';
+import 'bootstrap';
