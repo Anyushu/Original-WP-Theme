ba191abe4f1a8b184fbc4bdf9c0de8b83c885065
ShunyaSuzuki <anyushu2017@gmail.com>
2019-12-21 14:36:46 +0900

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,3 +9,22 @@ import './hljs';
 
 // loads the Icon plugin
 UIkit.use(Icons);
+
+// Google Fonts
+window.WebFontConfig = {
+  google: {
+    families: ['Noto+Sans+JP:300,500&display=swap&subset=japanese']
+  },
+  active: function() {
+    sessionStorage.fonts = true;
+  }
+};
+
+(function() {
+  var wf = document.createElement('script');
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
--- single.php
+++ single.php
@@ -2,12 +2,12 @@
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header();
+
 the_post();
 $post_id = get_the_ID();
 $ttl = get_the_title();
 $permalink = get_the_permalink();
 $cat = get_the_category();
-$tags = get_the_tags();
 $cat = $cat[0];
 $cat_id = $cat->term_id;
 $cat_name = $cat->name;
@@ -33,7 +33,7 @@ if (has_post_thumbnail()) {
 </div>
 <?php if ($img != ''): ?>
 <figure>
-<img data-src="<?php echo $img_m; ?>" alt="<?php echo $ttl; ?>" uk-img>
+<img class="uk-box-shadow-small" data-src="<?php echo $img_m; ?>" alt="<?php echo $ttl; ?>" uk-img>
 </figure>
 <?php endif; ?>
 <div id="toc" class="toc uk-section-muted"></div>
