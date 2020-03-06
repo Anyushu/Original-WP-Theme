8f97860768936f3c0e01c6f0d7d2e2991408010d
ShunyaSuzuki <anyushu2017@gmail.com>
2020-03-06 14:19:54 +0900

    update

--- header.php
+++ header.php
@@ -55,7 +55,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <div class="uk-offcanvas-bar uk-offcanvas-bar-animation uk-offcanvas-slide">
 <button class="uk-offcanvas-close uk-close uk-icon" type="button" uk-close></button>
 <ul class="uk-nav uk-nav-default uk-margin-bottom">
-<li class="uk-heading-divider">カテゴリー</li>
+<li class="uk-heading-divider uk-text-bold uk-margin-small-bottom">カテゴリー</li>
 <?php
 $args = [
     'orderby' => 'id',
@@ -67,16 +67,16 @@ foreach ($categories as $category):
     $cat_link = get_category_link($category->term_id);
     $cat_name = $category->name;
 ?>
-<li><a href="<?php echo $cat_link; ?>"><?php echo $cat_name; ?></a></li>
+<li><a class="uk-link-text" href="<?php echo $cat_link; ?>"><?php echo $cat_name; ?></a></li>
 <?php endforeach; ?>
 </ul>
 <ul class="uk-nav uk-nav-default">
-<li class="uk-heading-divider">ページ</li>
-<li><a href="<?php echo $home; ?>">トップ</a></li>
+<li class="uk-heading-divider uk-text-bold uk-margin-small-bottom">ページ</li>
+<li><a class="uk-link-text" href="<?php echo $home; ?>">トップ</a></li>
 <?php
 $pages = get_page_list();
 foreach ($pages as $key => $page): ?>
-<li><a href="<?php echo get_page_link($page->ID); ?>"><?php echo $page->post_title; ?></a></li>
+<li><a class="uk-link-text" href="<?php echo get_page_link($page->ID); ?>"><?php echo $page->post_title; ?></a></li>
 <?php endforeach; wp_reset_query(); ?>
 </ul>
 </div>
